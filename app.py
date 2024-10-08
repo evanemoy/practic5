3e902849aeac8c8a4dc8c8f9d0ec5f3b0aac61b2
unknown <nemoykina00@inbox.ru>
2024-10-08 09:37:25 +0300

    'sec'

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import os 
 
 app = Flask(__name__)
 
@@ -53,4 +54,5 @@ def delete_contact(id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render использует переменную PORT
+    app.run(host='0.0.0.0', port=port)
